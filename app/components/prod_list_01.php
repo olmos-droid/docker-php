fc5769243e02f008cf3f6898ff5f3b5815a66f36
josep <olmosescolatreball@gmail.com>
2021-10-04 04:32:25 +0200

    ficheros ok

--- app/components/prod_list_01.php
+++ app/components/prod_list_01.php
@@ -4,13 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product's List</title>
-    <script>
-function start() {
-    window.location.href="components/prod_list_01.php";
-}
-</script>
-<body onload="start()">
+    <title>prod_list_01</title>
+</head>
+<body>
+    <h1>prod_list_01</h1>
     
 </body>
 </html>
